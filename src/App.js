30c9b23fae2b8187e6c76c813a26c8fed4916218
Atte Ikola <atte.ikola@gmail.com>
2026-04-15 15:51:28 +0300

    Formatted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import logo from './pictures/NightShiftLogo.png';
 
 function App() {
   return (
     <div className="App">
       <header>
-      <t>Hello world</t>
+        <h1>You have been Jäynä'd</h1>
+        <p>By the International and Masters students of Aalto University</p>
+        <p>Sinut on Jäynätty Aalto yliopiston Kansainvälisten ja Maisteri opiskelijoiden toimesta</p>
+        <a href="https://www.jayna.fi/en" target="_blank" rel="noopener noreferrer">
+          More info here
+        </a>
       </header>
+      <img
+        src={logo}
+        className="App-logo"
+        alt="logo"
+        style={{ marginTop: '2rem', maxWidth: '120px', width: '100%', height: 'auto' }}
+      />
     </div>
   );
 }
